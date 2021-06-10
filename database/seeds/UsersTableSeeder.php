f5598b0e11c9e39b1441b6b2079ab0bb8237032a
zinky <minhtinhph2302@gmail.com>
2021-06-10 22:23:45 +0700

    add User Seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,7 +6,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -18,9 +17,17 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'o0ozinkyo0o@gmail.com',
+            'role' => 1,
+            'password' => Hash::make('minhtinh2807'),
+        ]);
+        DB::table('users')->insert([
             'name' => 'Tinh Pham',
             'username' => 'zinkyfmt',
-            'email' => 'o0ozinkyo0o@gmail.com',
+            'email' => 'minhtinh2302@gmail.com',
+            'role' => 5,
             'password' => Hash::make('minhtinh2807'),
         ]);
     }
