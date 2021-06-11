feeb764f70cce3086ff3d6dbda1775761f335d8a
zinky <minhtinhph2302@gmail.com>
2021-06-11 19:40:03 +0700

    m

--- resources/views/dashboard/base.blade.php
+++ resources/views/dashboard/base.blade.php
@@ -13,9 +13,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
-    <meta name="author" content="Łukasz Holeczek">
-    <meta name="keyword" content="Bootstrap,Admin,Template,Open,Source,jQuery,CSS,HTML,RWD,Dashboard">
+    <meta name="description" content="Euro 2020">
+    <meta name="author" content="Tinh Pham">
+    <meta name="keyword" content="Euro 2020">
     <title>Euro 2020</title>
     <link rel="apple-touch-icon" sizes="57x57" href="assets/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="assets/favicon/apple-icon-60x60.png">
