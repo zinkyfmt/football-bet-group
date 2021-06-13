6aa7e19311dd4eba309a7bcd6d9408b6e7453f50
zinky <minhtinhph2302@gmail.com>
2021-06-13 17:39:15 +0700

    m

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller
 // validate the info, create rules for the inputs
         $rules = array(
             'email'    => 'required|email', // make sure the email is an actual email
-            'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
+            'password' => 'required|min:6'
         );
 
         $validator = Validator::make(Input::all(), $rules);
@@ -62,7 +62,6 @@ class LoginController extends Controller
                 ->withErrors($validator) // send back all errors to the login form
                 ->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
         } else {
-
             // create our user data for the authentication
             $userdata = array(
                 'email'     => Input::get('email'),
